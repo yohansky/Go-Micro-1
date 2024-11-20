2d956b636de9e4a95baccfeab4d62c1c8a8262b2
Yohanes <yohanesdahlan@gmail.com>
2024-11-20 10:23:15 +0700

    setting up Logger data models

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -28,6 +29,17 @@ func main() {
 	}
 
 	client = mongoClient
+
+	// create a context in order to disconect
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	// close connection
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
 }
 
 func connectToMongo() (*mongo.Client, error) {
