e50df055ae60f91081c9bb69c1d5acce59bfd744
Yohanes <yohanesdahlan@gmail.com>
2024-11-20 16:57:09 +0700

    adding MongoDB to docker

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -15,7 +15,7 @@ import (
 const (
 	webPort  = "80"
 	rpcPort  = "5001"
-	mongoURL = "mongodb://mongo:27017"
+	mongoURL = "mongodb://mongo:27018"
 	grpcPort = "50001"
 )
 
@@ -50,20 +50,31 @@ func main() {
 	}
 
 	//start web server
-	go app.serve()
-}
-
-func (app *Config) serve() {
+	// go app.serve()
+	log.Println("Starting service on port", webPort)
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
 		Handler: app.routes(),
 	}
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Panic()
 	}
+
 }
+
+// func (app *Config) serve() {
+// 	srv := &http.Server{
+// 		Addr:    fmt.Sprintf(":%s", webPort),
+// 		Handler: app.routes(),
+// 	}
+
+// 	err := srv.ListenAndServe()
+// 	if err != nil {
+// 		log.Panic()
+// 	}
+// }
 
 func connectToMongo() (*mongo.Client, error) {
 	// create connection options
@@ -79,6 +90,8 @@ func connectToMongo() (*mongo.Client, error) {
 		log.Println("Error connecting :", err)
 		return nil, err
 	}
+
+	log.Println("Connected to mongo")
 
 	return c, nil
 }
