9a208552db04dcbd82301917da3805f8b62f323c
Yohanes <yohanesdahlan@gmail.com>
2024-11-25 11:35:59 +0700

    Listening for gRPC connection in Logger service

--- logger-service/cmd/api/grpc.go
+++ logger-service/cmd/api/grpc.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"log-service/data"
 	"log-service/logs"
+	"net"
+
+	"google.golang.org/grpc"
 )
 
 type LogServer struct {
@@ -29,4 +34,21 @@ func (l *LogServer) WriteLog(ctx context.Context, req *logs.LogRequest) (*logs.L
 	// return response
 	res := &logs.LogResponse{Result: "logged!"}
 	return res, nil
+}
+
+func (app *Config) gRPCListen() {
+	lis, err := net.Listen("tcp", fmt.Sprintf("%s", grpcPort))
+	if err != nil {
+		log.Fatal("Failed to listen for gRPC: %v", err)
+	}
+
+	s := grpc.NewServer()
+
+	logs.RegisterLogServiceServer(s, &LogServer{Models: app.Models})
+
+	log.Printf("gRPC Server started on port %s", grpcPort)
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatal("Failed to listen for gRPC: %v", err)
+	}
 }
